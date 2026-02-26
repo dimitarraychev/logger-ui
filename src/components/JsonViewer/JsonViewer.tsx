1ed1187ba72d82cfd4120a1c7a9803fdb5d2ae72
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-02-26 07:15:58 +0000

    updated json parsing

--- src/components/JsonViewer/JsonViewer.tsx
+++ src/components/JsonViewer/JsonViewer.tsx
@@ -2,16 +2,19 @@ import ReactJson from "react-json-view";
 import "./JsonViewer.css";
 
 interface JsonViewerProps {
-  jsonData: {};
+  jsonData: {} | string;
   title: string;
 }
 
 const JsonViewer = ({ jsonData, title }: JsonViewerProps) => {
+  const parsedJson =
+    typeof jsonData === "string" ? JSON.parse(jsonData) : jsonData;
+
   return (
     <div className="json-view-wrapper">
       <label className="json-title">{title}</label>
       <ReactJson
-        src={jsonData}
+        src={parsedJson}
         theme="summerfruit"
         style={{ backgroundColor: "#0d1421", fontSize: "14px" }}
         enableClipboard={true}
